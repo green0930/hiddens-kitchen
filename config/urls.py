92d874d6aca080856cffb9a5689a21cbf78e5586
Haejin <haejingreen@hotmail.com>
2026-08-31 14:35:41 -0700

    Add robots.txt for search engines

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.sitemaps import Sitemap
 from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path, reverse
+from django.http import HttpResponse
 
 
 class StaticViewSitemap(Sitemap):
@@ -28,6 +29,17 @@ urlpatterns = [
     path("admin/", admin.site.urls),
 
     path(
+            "robots.txt",
+            lambda request: HttpResponse(
+                "User-agent: *\n"
+                "Allow: /\n"
+                "Disallow: /admin/\n"
+                "Sitemap: https://hiddens-kitchen.onrender.com/sitemap.xml\n",
+                content_type="text/plain",
+            ),
+        ),
+
+    path(
         "sitemap.xml",
         sitemap,
         {"sitemaps": sitemaps},
@@ -48,4 +60,5 @@ urlpatterns = [
         "",
         include("core.urls"),
     ),
+
 ]
