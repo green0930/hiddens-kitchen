f05265beed6b65da8a6519151f372d96477287bc
Haejin <haejingreen@hotmail.com>
2026-08-30 18:51:49 -0700

    Create production admin during deployment

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-
-#!/usr/bin/env bash
 set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
 python manage.py loaddata menu_items
+
+python manage.py createsuperuser --noinput || true
