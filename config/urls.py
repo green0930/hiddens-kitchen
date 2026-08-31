c2d0b654433f1b0d7ff01ede6a24e6b77bf8685c
Haejin <haejingreen@hotmail.com>
2026-08-31 13:11:05 -0700

    Add XML sitemap for Google Search

--- config/urls.py
+++ config/urls.py
@@ -1,20 +1,49 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.contrib.sitemaps import Sitemap
+from django.contrib.sitemaps.views import sitemap
+from django.urls import include, path, reverse
+
+
+class StaticViewSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.8
+
+    def items(self):
+        return [
+            "home",
+            "menu:menu_list",
+            "orders:preorder",
+        ]
+
+    def location(self, item):
+        return reverse(item)
+
+
+sitemaps = {
+    "static": StaticViewSitemap,
+}
 
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
+
     path(
-        "admin/",
-        admin.site.urls,
+        "sitemap.xml",
+        sitemap,
+        {"sitemaps": sitemaps},
+        name="django.contrib.sitemaps.views.sitemap",
     ),
+
     path(
         "menu/",
         include("menu.urls"),
     ),
+
     path(
         "order/",
         include("orders.urls"),
     ),
+
     path(
         "",
         include("core.urls"),
